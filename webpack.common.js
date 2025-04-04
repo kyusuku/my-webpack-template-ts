db5db06d092ed17d0daf940e469c7229b9673324
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-05 01:50:50 +0700

    Configure Webpack template for TypeScript

--- webpack.common.js
+++ webpack.common.js
@@ -2,31 +2,40 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
+      template: "./src/template.html",
+      scriptLoading: "defer",
     }),
   ],
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.html$/i,
-            loader: "html-loader",
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-        },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,8 @@ module.exports = merge(common, {
     mode: "development",
     devtool: "eval-source-map",
     devServer: {
+        static: "./dist", 
         watchFiles: ["./src/template.html"],
+        open: true,
     },
 });
